c5d4776ea808da4c186d73a3a2c0746ddc090e2b
Elkin2234513 <126176086+Elkin2234513@users.noreply.github.com>
2023-03-26 20:12:41 -0500

    Add files via upload

--- PIEDRA_PAPEL_O_TIJERA.py
+++ PIEDRA_PAPEL_O_TIJERA.py
@@ -1,29 +1,48 @@
 import random
 
 print("---------------------------")
-print("---PIEFRA PAPEL O TIJERA---")
+print("---------------------------")
+print("---------------------------")
+print("---PIEDRA PAPEL O TIJERA---")
+print("---------------------------")
+print("---------------------------")
+print("---------------------------")
+print("---------------------------")
+print("-------PIEDRA = 1----------")
+print("-------PAPEL =  2----------")
+print("-------TIJERA = 3----------")
+print("---------------------------")
 print("---------------------------")
 
-#input
-aleatorio = random.randint(0, 3)
-elijePC = ""
-print("1. piedra")
-print("2. papel")
-print("3. tijera")
-opcion = int(input("Elige tu opcion: "))
 
-
-#processing
-
-if opcion == 1:
-    elijePC = "piedra"
-elif opcion == 2:
-    elijePC == "papel"
-elif opcion == 3: 
-    elijePC == "Tijera"
-print("Elejiste: ", elijePC)
+usuario = int(input("DIGITE UN NUMERO: "))
+PC = random.randint(1,3)
 
 
 
+if usuario == 1 and PC == 2:
+    print("GANASTE") 
+else:
+    if usuario == PC:
+        print("EMPATE")
+    else:
+        if usuario == 2 and PC == 3:
+            print("PERDISTE")
+        else: 
+            if usuario == 3 and PC == 1:
+                print("PERDISTE")  
+            else:
+                if usuario == 2 and PC == 1:
+                    print("GANASTE")
+                else:
+                    if usuario == 3 and PC == 2:
+                        print("GANASTE")
+                    else:
+                        print("PERDISTE")
 
-#output
+
+
+
+
+
+
